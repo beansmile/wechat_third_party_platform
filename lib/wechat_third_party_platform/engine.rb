53728622b2ff0c3f9ccdd70d98bd44c2ece4c6f5
jane <jane@beansmile.com>
2020-10-23 01:17:06 +0800

    fix: add assets precompile

--- lib/wechat_third_party_platform/engine.rb
+++ lib/wechat_third_party_platform/engine.rb
@@ -1,5 +1,10 @@
 module WechatThirdPartyPlatform
   class Engine < ::Rails::Engine
     isolate_namespace WechatThirdPartyPlatform
+
+
+    initializer "wechat_third_party_platform.assets.precompile" do |app|
+      app.config.assets.precompile += %w( wechat_third_party_platform/application.css )
+    end
   end
 end
