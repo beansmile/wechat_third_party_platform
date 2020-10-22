06bb424dbaa98fd70202b28fba593dfeb73f1cbc
MC <mc@beansmile.com>
2020-10-22 16:07:28 +0800

    feat: 微信事件推送

---
+++ lib/generators/wechat_third_party_platform/templates/wechat_controller.rb
@@ -0,0 +1,6 @@
+WechatThirdPartyPlatform::WechatController.class_eval do
+  # 这里放需要重写的推送事件
+  # 如：
+  # def weapp_audit_success_handler
+  # end
+end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,6 +7,12 @@ require "wechat_third_party_platform"
 
 module Dummy
   class Application < Rails::Application
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
