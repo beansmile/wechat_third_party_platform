95c02a6a6ac76c674c7b38e97155be5bc43aadd6
MC <mc@beansmile.com>
2020-10-24 15:52:00 +0800

    fix: 移除Template model

--- app/models/wechat_third_party_platform/submition.rb
+++ app/models/wechat_third_party_platform/submition.rb
@@ -1,6 +1,5 @@
 module WechatThirdPartyPlatform
   class Submition < ApplicationRecord
-    belongs_to :tempalte
     belongs_to :application, class_name: WechatThirdPartyPlatform.application_class_name
   end
 end
