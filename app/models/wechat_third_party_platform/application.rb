db0d0c2c12328fc1203fa5e6038255deff4011a4
MC <mc@beansmile.com>
2020-10-26 15:13:23 +0800

    feat: 第三方平台应用管理
    https://trello.com/c/w2pC9X4s/238-2第三方平台应用管理

--- app/models/wechat_third_party_platform/application.rb
+++ app/models/wechat_third_party_platform/application.rb
@@ -2,5 +2,29 @@
 
 module WechatThirdPartyPlatform
   class Application < ApplicationRecord
+    enum account_type: {
+      # 订阅号暂不处理
+      # 公众号暂不处理
+      # official: 2,
+      # 小程序
+      mini_program: 3
+    }, _suffix: true
+
+    enum principal_type: {
+      # 个人
+      person: 0,
+      # 企业
+      enterprise: 1,
+      # 媒体
+      media: 2,
+      # 政府
+      government: 3,
+      # 其他
+      other: 4
+    }, _suffix: true
+
+    def client
+      @client ||= WechatThirdPartyPlatform::MiniProgramClient.new(appid, access_token)
+    end
   end
 end
