c9e54c42fc6a32dcd9db09c71279105012740a56
MC <mc@beansmile.com>
2020-10-26 17:58:48 +0800

    feat: 小程序代码提交审核
    https://trello.com/c/XqZvZukX/230-2小程序代码提交审核

--- app/models/wechat_third_party_platform/application.rb
+++ app/models/wechat_third_party_platform/application.rb
@@ -23,10 +23,58 @@ module WechatThirdPartyPlatform
       other: 4
     }, _suffix: true
 
+    belongs_to :audit_submition, class_name: "WechatThirdPartyPlatform::Submition", optional: true
     has_many :testers, dependent: :destroy
 
     def client
       @client ||= WechatThirdPartyPlatform::MiniProgramClient.new(appid, access_token)
+    end
+
+    def commit(template_id:, user_version:, user_desc:, ext_json: {})
+      errors.add(:base, "已有正在审核的代码") and return false if audit_submition && (audit_submition.pending? || audit_submition.delay?)
+
+      response = client.commit(
+        template_id: template_id,
+        user_version: user_version,
+        user_desc: user_desc,
+        ext_json: ext_json.to_json
+      )
+
+      errors.add(:base, response["errmsg"]) and return false unless response["errcode"] == 0
+
+      self.audit_submition = Submition.new(
+        template_id: template_id,
+        ext_json: ext_json,
+        user_version: user_version,
+        user_desc: user_desc,
+        application: self
+      )
+
+      save
+    end
+
+    def submit_audit
+      errors.add(:base, "请先上传代码") and return false unless audit_submition
+      errors.add(:base, "已有正在审核的代码") and return false if audit_submition.pending? || audit_submition.delay?
+
+      # TODO 后期需要支持item_list，preview_info，version_desc等参数
+      # TODO 小程序尚未完善暂时统一返回成功
+      response = if Rails.env.production?
+                   client.submit_audit
+                 else
+                   {
+                     "errcode" => 0,
+                     "errmsg" => "ok",
+                     "auditid" => Time.current.to_i
+                   }
+                 end
+
+      errors.add(:base, response["errmsg"]) and return false unless response["errcode"] == 0
+
+      audit_submition.update(auditid: response["auditid"])
+      audit_submition.pending!
+
+      true
     end
   end
 end
--- app/models/wechat_third_party_platform/submition.rb
+++ app/models/wechat_third_party_platform/submition.rb
@@ -3,5 +3,19 @@
 module WechatThirdPartyPlatform
   class Submition < ApplicationRecord
     belongs_to :application, class_name: "WechatThirdPartyPlatform::Application"
+    has_one :audit_application, class_name: "WechatThirdPartyPlatform::Application", foreign_key: :audit_submition_id, dependent: :nullify
+
+    enum state: {
+      # 待提交审核
+      init: 0,
+      # 审核中
+      pending: 1,
+      # 审核通过
+      success: 2,
+      # 审核不通过
+      fail: 3,
+      # 审核延后
+      delay: 4
+    }
   end
 end
