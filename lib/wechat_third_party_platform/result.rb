f2639c676acdb292d2f935818a0170f84f612eef
leif <leif@beansmile.com>
2020-11-09 18:25:59 +0800

    feat: format wechat error message

---
+++ lib/wechat_third_party_platform/result.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module WechatThirdPartyPlatform
+  class Result < ::Hash
+    @@wechat_error_codes = YAML.load_file("#{WechatThirdPartyPlatform::Engine.root}/config/wechat_error_codes.yml")
+
+    SUCCESS_FLAG = 'ok'.freeze
+
+    def initialize(result)
+      result.each_pair do |k, v|
+        self[k] = v
+      end
+    end
+
+    def success?
+      self["errcode"] == 0 && self["errmsg"] == SUCCESS_FLAG
+    end
+
+    def cn_msg
+      "wtpp: #{@@wechat_error_codes[self["errcode"]] || self["errmsg"]}"
+    end
+  end
+end
