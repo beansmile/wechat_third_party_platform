ba1ad6ec57c4db3f1b5c7742da4d43dd3487cab9
MC <mc@beansmile.com>
2020-11-13 18:03:34 +0800

    fix: 绑定体验者失败
    https://trello.com/c/Ik0WwUPd/516-绑定体验者失败

--- lib/wechat_third_party_platform/grape_api/entities/model.rb
+++ lib/wechat_third_party_platform/grape_api/entities/model.rb
@@ -10,5 +10,31 @@ module WechatThirdPartyPlatform::GrapeAPI::Entities
     expose :tn do |obj|
       obj.class.table_name
     end
+
+    def self.guess_model
+      begin
+        model_name = "WechatThirdPartyPlatform::#{self.to_s.split("::")[-1].singularize}"
+
+        model = Object.const_get(model_name)
+      rescue NameError => e
+        # 可能有一个 User entity，还有一个 UserDetail entity 表示更详细的信息
+        namespaces = model_name.split("::")[0..-2]
+        model_name = model_name.split("::")[-1]
+
+        if model_name.match(/.*Detail$/)
+          model_name = (namespaces + [model_name.slice(0..-7)]).join("::")
+
+          model = Object.const_get(model_name)
+        elsif model_name.match(/^Simple.*$/)
+          model_name = (namespaces + [model_name.slice(6..-1)]).join("::")
+
+          model = Object.const_get(model_name)
+        end
+      rescue StandardError => e
+        # 什么都不处理
+      end
+
+      model
+    end
   end
 end
