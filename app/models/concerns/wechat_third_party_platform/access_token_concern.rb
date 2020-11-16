079976df43ab53d223b7773f5ec835553cbb4edf
jane <jane@beansmile.com>
2020-11-16 16:13:14 +0800

    fix: access_token is missing in staging env
    https://trello.com/c/EypSIOuY/523-1-添加体验者失败

--- app/models/concerns/wechat_third_party_platform/access_token_concern.rb
+++ app/models/concerns/wechat_third_party_platform/access_token_concern.rb
@@ -4,7 +4,7 @@ module WechatThirdPartyPlatform
   module AccessTokenConcern
     extend ActiveSupport::Concern
 
-    ENV_FALLBACK_ARRAY = [:production, :staging, :development]
+    ENV_FALLBACK_ARRAY = [[:production, :staging], [:development]]
 
     def self.included(klass)
       klass.instance_eval do
@@ -22,29 +22,36 @@ module WechatThirdPartyPlatform
       update!(access_token: get_access_token)
     end
 
+    # 正式环境和测试环境绑定的是不同的小程序，所以可以刷新access_token
+    # development环境获取access_token时，先去production获取，没获取到的话就去staing获取
     def get_access_token
       token = access_token
-      ENV_FALLBACK_ARRAY.each do |env|
-        if Rails.env == env.to_s
+      ENV_FALLBACK_ARRAY.each do |env_array|
+        if Rails.env.to_sym.in?(env_array)
           break
         else
-          host = Rails.application.credentials.dig(env, host_key)
+          env_array.each do |env|
+            host = Rails.application.credentials.dig(env, host_key)
 
-          # 未部署的环境暂时不配置host
-          next if host.blank?
+            # 未部署的环境暂时不配置host
+            next if host.blank?
 
-          resp = HTTParty.get("#{host}/admin_api/v1/wechat_third_party_platform/applications/access_token?appid=#{appid}", headers: { "api-authorization-token" => Rails.application.credentials.dig(env, self.class.api_authorization_token_key) })
-          next unless token = resp["access_token"]
+            resp = HTTParty.get("#{host}/admin_api/v1/wechat_third_party_platform/applications/access_token?appid=#{appid}", headers: { "api-authorization-token" => Rails.application.credentials.dig(env, self.class.api_authorization_token_key) })
+            next unless token = resp["access_token"]
+
+            break
+          end
         end
       end
       token
     end
 
-    # 只有正式环境可以刷新access_token, 未部署正式前只有测试环境可以刷新
     # 没授权成功（即没绑定成功）的小程序不可刷新access_token
     def can_refresh_access_token?
-      ENV_FALLBACK_ARRAY.each do |env|
-        return !!(Rails.env.send("#{env}?") && refresh_token && project_application.present?) if Rails.application.credentials.dig(env, host_key)
+      ENV_FALLBACK_ARRAY.each do |env_array|
+        if env_array.any? { |env| Rails.application.credentials.dig(env, host_key) }
+          return Rails.env.to_sym.in?(env_array) && refresh_token && project_application.present?
+        end
       end
     end
 
